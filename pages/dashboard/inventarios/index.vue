5e505a58ca377381aaa067d4ff0d03361e878fa1
unknown <f.santacruzhernandezfrancisco>
2024-11-28 21:50:30 -0600

    inventarios front

---
+++ pages/dashboard/inventarios/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <inventarios-crud />
+  </template>
+  
+  <script>
+  import inventariosCrud from '../../../components/inventariosCrud.vue'
+  export default {
+    components: {
+      inventariosCrud
+    },
+    layout: 'dashboard',
+    middleware: 'auth'
+  }
+  </script>
+  
+    <style scoped>
+    </style>
